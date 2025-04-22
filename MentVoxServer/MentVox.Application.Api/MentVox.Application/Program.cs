f998e08b90ec417e4604aa3bb98a9b720ad29788
RuthCo6 <rc6796580@gmail.com>
2025-04-22 16:25:51 +0300

    Two pages to fix it

--- MentVoxServer/MentVox.Application.Api/MentVox.Application/Program.cs
+++ MentVoxServer/MentVox.Application.Api/MentVox.Application/Program.cs
@@ -32,7 +32,6 @@ builder.Services.AddScoped<IChatGptService, ChatGptService>();
 builder.Services.AddScoped<IElevenLabsService, ElevenLabsService>(); 
 builder.Services.AddScoped<IConversationService, ConversationService>();
 
-builder.Services.AddScoped<ConversationService>();
 
 
 // Configure JWT Authentication
@@ -100,8 +99,17 @@ builder.Services.AddHttpClient<ElevenLabsService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 
